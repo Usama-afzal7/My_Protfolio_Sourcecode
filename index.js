8bc5d5f4b4ee4f7383d9fce720be52ff8aa2a00d
usama-afzal7 <afzalusama77@gmail.com>
2023-09-23 03:34:23 +0500

    Committed

--- index.js
+++ index.js
@@ -26,3 +26,29 @@ function toggleModal(){
     isModalOpen = true;
     document.body.classList += " modal--open";
 }
+
+let contrastToggle = false;
+
+function toggleContrast(){
+    contrastToggle = !contrastToggle
+    if (contrastToggle){
+        document.body.classList += " dark-theme"
+    }
+    else{
+        document.body.classList.remove("dark-theme")
+    }
+}
+
+const scaleFactor = 1 / 20;
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+  
+    for (let i = 0; i < shapes.length; ++i) {
+      const isOdd = i % 2 !== 0;
+      const boolInt = isOdd ? -1 : 1;
+      // Added rotate after tutorial
+      shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+    }
+  }
